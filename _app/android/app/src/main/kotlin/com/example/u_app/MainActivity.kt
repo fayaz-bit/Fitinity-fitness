bb2851286d9fd2865b8b2d5a31245352d571e299
fayaz ak <fayazak032@gmail.com.com>
2026-03-11 19:10:23 +0530

    manifest issues

--- _app/android/app/src/main/kotlin/com/example/u_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.fitinity.fitness
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ _app/android/app/src/main/kotlin/fitnity/fitnessapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.fitinity.fitnessapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
